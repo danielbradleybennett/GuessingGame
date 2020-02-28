c8141b8ae89eca38aa7af2ee4915c25e4c6010ee
DanielBradleyBennett <danielbradleybennett@gmail.com>
2020-02-28 09:31:48 -0600

    while loop

--- Program.cs
+++ Program.cs
@@ -1,23 +1,34 @@
 ﻿using System;
 
-namespace GuessingGame {
-  class Program {
-    static void Main (string[] args) {
+namespace GuessingGame
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
 
-      Random rand = new Random ();
-      int secretNumber = rand.Next (1, 11);
+      Random rand = new Random();
+      int secretNumber = rand.Next(1, 11);
 
-      for (int i = 0; i < 3; i++) {
 
-        Console.WriteLine ("Guess a number between 1 & 10");
-        string userGuess = Console.ReadLine ();
-        int userGuessInt = int.Parse (userGuess);
+      // for (int i = 0; i < 3; i++) 
 
-        if (userGuessInt == secretNumber) {
-          Console.WriteLine ($"You guessed it! It was {secretNumber}");
-          return;
-        } else {
-          Console.WriteLine ("Nope that wasnt it");
+      while (true)
+      {
+
+        Console.WriteLine("Guess a number between 1 & 10");
+        string userGuess = Console.ReadLine();
+        int userGuessInt = int.Parse(userGuess);
+
+        if (userGuessInt == secretNumber)
+        {
+          Console.WriteLine($"You guessed it! It was {secretNumber}");
+          break;
+          // return;
+        }
+        else
+        {
+          Console.WriteLine("Nope that wasnt it");
         }
 
       }
